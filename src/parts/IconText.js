f6239a82c2b26fa93b37f3d4ec483c19470e4d22
denzord <45736463+denzord@users.noreply.github.com>
2021-03-10 07:53:23 +0700

    Update IconText.js

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -4,7 +4,7 @@ import Button from "elements/Button";
 
 export default function IconText() {
   return (
-    <Button className="brand-text-icon ml-2" href="" type="link">
+    <Button className="brand-text-icon" href="" type="link">
       Yuk<span className="text-gray-900">Maen</span>
     </Button>
   );
